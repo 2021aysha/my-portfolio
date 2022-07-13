e1522dcfbbd04a040ab674fe3110da2e93d84641
Eliyas Hossain <eliyashossain700@gmail.com>
2022-07-13 19:54:43 +0600

    Swiper responsive

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,11 @@
 import React, { useContext } from "react";
 import "./Portfolio.css";
-import { Swiper, SwiperSlide } from "swiper/react"
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
 import "swiper/css";
+import project1 from "../../img/project-2.png";
+import project2 from "../../img/project-1.png";
+import project3 from "../../img/project-3.png";
 import Sidebar from "../../img/sidebar.png";
 import Ecommerce from "../../img/ecommerce.png";
 import HOC from "../../img/hoc.png";
@@ -13,28 +17,50 @@ const Portfolio = () => {
   return (
     <div className="portfolio" id="portfolio">
       {/* heading */}
-      <span style={{color: darkMode?'white': ''}}>Recent Projects</span>
+      <span style={{ color: darkMode ? "white" : "" }}>Recent Projects</span>
       <span>Portfolio</span>
 
       {/* slider */}
       <Swiper
-        spaceBetween={30}
-        slidesPerView={3}
+        freeMode={true}
         grabCursor={true}
+        modules={[FreeMode]}
         className="portfolio-slider"
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 65,
+          },
+          1280: {
+            slidesPerView: 3,
+            spaceBetween: 65,
+          },
+        }}
       >
         <SwiperSlide>
-          <img src={Sidebar} alt="" />
+          <img src={project1} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Ecommerce} alt="" />
+          <img src={project2} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={MusicApp} alt="" />
+          <img src={project3} alt="" />
         </SwiperSlide>
-        <SwiperSlide>
+        {/* <SwiperSlide>
           <img src={HOC} alt="" />
-        </SwiperSlide>
+        </SwiperSlide> */}
       </Swiper>
     </div>
   );
